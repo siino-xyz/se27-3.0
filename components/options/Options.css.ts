a7b0534174d9be2bcfc8751668564043456b1fdb
siino-xyz <b114807h@gmail.com>
2022-11-01 02:50:31 +0900

    text.css.tsを削除

--- components/options/Options.css.ts
+++ components/options/Options.css.ts
@@ -1,33 +1,24 @@
 import { style } from "@vanilla-extract/css";
-import {
-  text,
-  heading_large,
-  heading_medium,
-  heading_small,
-} from "@styles/text.css";
 import { fonts } from "@styles";
 
 export const styles = {
-  paragraph: style([{}, text]),
+  paragraph: style([{}]),
   heading2: style([
     {
       color: "red",
       fontFamily: fonts.heading,
     },
-    heading_large,
   ]),
   heading3: style([
     {
       color: "green",
       fontFamily: fonts.heading,
     },
-    heading_medium,
   ]),
   heading4: style([
     {
       color: "purple",
       fontFamily: fonts.heading,
     },
-    heading_small,
   ]),
 };
--- styles/index.ts
+++ styles/index.ts
@@ -1,6 +1,5 @@
 export * from "./globals.css";
 export * from "./preflight.css";
 export * from "./variables.css";
-export * from "./text.css";
 export * from "./theme.css";
 export * from "./sprinkles.css";
